13020f59e7b2db15035d6eab54afb745fc5ae9bb
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-06-26 23:06:29 +0300

    add more prod

--- script.js
+++ script.js
@@ -72,3 +72,195 @@ form.addEventListener('submit', function (e) {
 //     document.removeEventListener('click', outsideClickHandler); // Удаляем слушатель кликов, так как модальное окно закрыто
 //   }
 // }
+
+
+
+
+  var products = [
+    '<li class="item-product"><div class="image-container">' +
+    '<a class="item-link" data-fancybox="gallery" href="./img/infinity/inf-prod-tumb.jpg">' +
+    '<img src="./img/infinity/inf-prod-tumb.jpg" alt="" class="item-img" />' +
+    '<div>' +
+    '<ul class="item-description">' +
+    '<li class="desc-container">' +
+    '<p>Колір: <span class="desc-type">Сірий</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Розмір: <span class="desc-type">150х240</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Матеріал: <span class="desc-type">Цегла</span></p>' +
+    '</li>' +
+    '</ul>' +
+    '</div>' +
+    '</a>' +
+    '</div>' +
+    '<div style="display: none;">' +
+    '<a href="./img/infinity/infinity-prod1.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod2.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod3.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod4.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod5.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod6.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod7.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod8.jpg" data-fancybox="gallery"></a>' +
+    '</div>' +
+    '<p class="item-type">Лежанка</p>' +
+    '<p class="item-name">Infiniti</p>' +
+    '<img src="./img/stars.png" alt=""  class="img-stars"/>' +
+    '<p class="price">2390 UAH</p>' +
+    '<button class="btn-order">' +
+    '<svg class="icon-order">' +
+    '<use xlink:href="./img/symbol-defs.svg#icon-shopping_bag"></use>' +
+    '</svg>' +
+    '</button></li>',
+    '<li class="item-product"><div class="image-container">' +
+    '<a class="item-link" data-fancybox="gallery" href="./img/infinity/inf-prod-tumb.jpg">' +
+    '<img src="./img/infinity/inf-prod-tumb.jpg" alt="" class="item-img" />' +
+    '<div>' +
+    '<ul class="item-description">' +
+    '<li class="desc-container">' +
+    '<p>Колір: <span class="desc-type">Сірий</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Розмір: <span class="desc-type">150х240</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Матеріал: <span class="desc-type">Цегла</span></p>' +
+    '</li>' +
+    '</ul>' +
+    '</div>' +
+    '</a>' +
+    '</div>' +
+    '<div style="display: none;">' +
+    '<a href="./img/infinity/infinity-prod1.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod2.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod3.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod4.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod5.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod6.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod7.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod8.jpg" data-fancybox="gallery"></a>' +
+    '</div>' +
+    '<p class="item-type">Лежанка</p>' +
+    '<p class="item-name">Infiniti</p>' +
+    '<img src="./img/stars.png" alt=""  class="img-stars"/>' +
+    '<p class="price">2390 UAH</p>' +
+    '<button class="btn-order">' +
+    '<svg class="icon-order">' +
+    '<use xlink:href="./img/symbol-defs.svg#icon-shopping_bag"></use>' +
+    '</svg>' +
+    '</button></li>',
+    '<li class="item-product"><div class="image-container">' +
+    '<a class="item-link" data-fancybox="gallery" href="./img/infinity/inf-prod-tumb.jpg">' +
+    '<img src="./img/infinity/inf-prod-tumb.jpg" alt="" class="item-img" />' +
+    '<div>' +
+    '<ul class="item-description">' +
+    '<li class="desc-container">' +
+    '<p>Колір: <span class="desc-type">Сірий</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Розмір: <span class="desc-type">150х240</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Матеріал: <span class="desc-type">Цегла</span></p>' +
+    '</li>' +
+    '</ul>' +
+    '</div>' +
+    '</a>' +
+    '</div>' +
+    '<div style="display: none;">' +
+    '<a href="./img/infinity/infinity-prod1.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod2.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod3.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod4.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod5.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod6.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod7.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod8.jpg" data-fancybox="gallery"></a>' +
+    '</div>' +
+    '<p class="item-type">Лежанка</p>' +
+    '<p class="item-name">Infiniti</p>' +
+    '<img src="./img/stars.png" alt=""  class="img-stars"/>' +
+    '<p class="price">2390 UAH</p>' +
+    '<button class="btn-order">' +
+    '<svg class="icon-order">' +
+    '<use xlink:href="./img/symbol-defs.svg#icon-shopping_bag"></use>' +
+    '</svg>' +
+    '</button></li>',
+    '<li class="item-product"><div class="image-container">' +
+    '<a class="item-link" data-fancybox="gallery" href="./img/infinity/inf-prod-tumb.jpg">' +
+    '<img src="./img/infinity/inf-prod-tumb.jpg" alt="" class="item-img" />' +
+    '<div>' +
+    '<ul class="item-description">' +
+    '<li class="desc-container">' +
+    '<p>Колір: <span class="desc-type">Сірий</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Розмір: <span class="desc-type">150х240</span></p>' +
+    '</li>' +
+    '<li class="desc-container">' +
+    '<p>Матеріал: <span class="desc-type">Цегла</span></p>' +
+    '</li>' +
+    '</ul>' +
+    '</div>' +
+    '</a>' +
+    '</div>' +
+    '<div style="display: none;">' +
+    '<a href="./img/infinity/infinity-prod1.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod2.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod3.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod4.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod5.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod6.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod7.jpg" data-fancybox="gallery"></a>' +
+    '<a href="./img/infinity/infinity-prod8.jpg" data-fancybox="gallery"></a>' +
+    '</div>' +
+    '<p class="item-type">Лежанка</p>' +
+    '<p class="item-name">Infiniti</p>' +
+    '<img src="./img/stars.png" alt=""  class="img-stars"/>' +
+    '<p class="price">2390 UAH</p>' +
+    '<button class="btn-order">' +
+    '<svg class="icon-order">' +
+    '<use xlink:href="./img/symbol-defs.svg#icon-shopping_bag"></use>' +
+    '</svg>' +
+    '</button></li>',
+    
+    
+    // Добавьте дополнительные HTML-коды товаров здесь
+  ];
+  function attachEventToButtons() {
+    const orderBtn = document.querySelectorAll('.btn-order');
+    
+    orderBtn.forEach(function (orderBtn) {
+      orderBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.style.display = 'block';
+        modal2.style.display = 'none';
+        // Ваш код для обработки клика на кнопку
+      });
+    });
+  }
+
+  var loadButton = document.getElementById('loadButton');
+  loadButton.addEventListener('click', function() {
+    var productList = document.querySelector('.product-container');
+    var ul = document.createElement('ul');
+    ul.className = 'product-container';
+    ul.setAttribute('data-category', 'furniture');
+  
+    products.forEach(function(product) {
+      var li = document.createElement('li');
+      li.innerHTML = product;
+      ul.appendChild(li);
+    });
+  
+    productList.insertAdjacentElement('afterend', ul);
+  
+    // Скрыть кнопку
+    loadButton.style.display = 'none';
+    attachEventToButtons();
+  });
+  
+
+  
